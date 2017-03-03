fd4686a7035be748b7c66940fea6c071493e1842
2881099 <2881099@qq.com>
2017-03-03 11:28:45 +0800

    test

---
+++ src/Module/Search/MroomController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using NpgsqlTypes;
+using aaa.BLL;
+using aaa.Model;
+
+namespace aaa.Module.Admin.Controllers2 {
+	[Route("[controller]")]
+	public class Mroom222333Controller : BaseController {
+		public Mroom222333Controller(ILogger<Mroom222333Controller> logger) : base(logger) { }
+
+		[HttpGet]
+		public APIReturn test() {
+			return APIReturn.参数格式不正确;
+		}
+
+		[HttpGet("test2")]
+		public APIReturn test2() {
+			return APIReturn.成功.SetData(Mroom.Select.ToList());
+		}
+	}
+}
